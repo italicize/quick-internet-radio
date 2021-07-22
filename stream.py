667ce1eb232d9a4d2314ea18734fba053d01878f
Jay Martin <italicize@gmail.com>
2021-07-21 23:59:14 -0700

    Add stream() and confirm() functions

--- stream.py
+++ stream.py
@@ -6,56 +6,61 @@ from omxplayer import OMXPlayer
 # Waits two minutes while the Raspberry Pi boots and connects to Wi-Fi.
 sleep(120)
 
-# Specifies URLs and where to output the sound.
+# Specifies URLs.
 url1 = 'http://ice2.somafm.com/thistle-128-mp3'
 url2 = 'http://ice6.somafm.com/thistle-128-mp3'
 url3 = 'http://ice4.somafm.com/thistle-128-mp3'
 url4 = 'http://ice1.somafm.com/thistle-128-mp3'
-output = 'alsa:hw:1,0'
-# In the output variable, try a 1 for a RPi Zero W. Try a 2 for a RPi 4.
-# To check the card numbers of audio devices, type aplay -l in Terminal.
+
+# Specifies the player and the audio out device.
+def stream():
+    """Play the stream at a url. Wait 30 seconds."""
+    player = OMXPlayer(url, args=['-o alsa:hw:1,0'])
+    sleep(30)
+# In the argument, try a 1 for a R Pi Zero W. Try a 2 for a R Pi 4.
+# To find the number of an audio out device, type aplay -l in Terminal.
+
+def confirm():
+    """Confirm that the player is playing. Wait 10 seconds."""
+    player.is_playing()
+    sleep(10)
 
 # Starts playing.
 url = url1
-player = OMXPlayer(url, args=['-o', output])
-sleep(30)
+stream()
 
 # Checks whether playing.
 while True:
   try:
-    player.is_playing()
-    sleep(10)
+    confirm()
   except:
-#   If not playing, tries to start playing again.
+#   If not playing, tries the same url again.
     try:
-      url = url1
-      player = OMXPlayer(url, args=['-o', output])
-      sleep(30)
-      player.is_playing()
-      sleep(10)
+      stream()
+      confirm()
     except:
-#     If url1 doesn't play, tries url2.
+#     If not playing, tries url1.
       try:
-        url = url2
-        player = OMXPlayer(url, args=['-o', output])
-        sleep(30)
-        player.is_playing()
-        sleep(10)
+        url = url1
+        stream()
+        confirm()
       except:
-#       Tries url3.
+#       Tries url2.
         try:
-          url = url3
-          player = OMXPlayer(url, args=['-o', output])
-          sleep(30)
-          player.is_playing()
-          sleep(10)
+          url = url2
+          stream()
+          confirm()
         except:
-#         Tries url4.
+#         Tries url3.
           try:
-            url = url4
-            player = OMXPlayer(url, args=['-o', output])
-            sleep(30)
-            player.is_playing()
-            sleep(10)
+            url = url3
+            stream()
+            confirm()
           except:
-            sleep(10)
+#           Tries url4.
+            try:
+              url = url4
+              stream()
+              confirm()
+            except:
+              sleep(10)
